21d1313c9765b10a4aeba5d0d47b6ea1b443ba7f
radiolok <radiolok@yandex.ru>
2015-03-04 01:02:35 +0300

    INIT: repository

---
+++ src/rf_tcp_protocol.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : rf_tcp_protocol.cpp
+// Author      : Artem Kaskanoc
+// Version     :
+// Copyright   : 
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "345" << endl; // prints 345
+	return 0;
+}
